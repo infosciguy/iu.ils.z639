84576abb64c524d811cb2f3c3aec0b943efce61f
infosciguy <jordan.reifsteck@gmail.com>
2016-03-30 19:19:05 -0400

    Open a CSV file
    I wrote this code to open a csv file containing data for project #2

--- opencsvtest.py
+++ opencsvtest.py
@@ -13,5 +13,4 @@ with open(r"C:\Users\Jordan\Desktop\emot-tweets2.csv", mode="rt", encoding = "UT
     EmotionTweets = collections.namedtuple('tweetID', 'screenName, userName, tweet, noisy_label', rename=True)  # get names from column headers
     for data in imap(EmotionTweets._make, reader):
         print(EmotionTweets.tweet)
-        # ...further processing of a line...
-
+        # ...further processing of a line....
